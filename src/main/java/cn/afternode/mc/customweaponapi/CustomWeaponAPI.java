aed1dcdd0c228de1735ec418f67dfd1577077326
tumuidle <7367204+tumuidle@user.noreply.gitee.com>
2022-07-15 00:00:34 +0800

    init

---
+++ src/main/java/cn/afternode/mc/customweaponapi/CustomWeaponAPI.java
@@ -0,0 +1,17 @@
+package cn.afternode.mc.customweaponapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomWeaponAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
